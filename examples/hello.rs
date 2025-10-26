83db06534a7c3f2756c1346e7599a1dfdb0146a3
Berkay Dinç <berkayydinc@gmail.com>
2025-10-26 16:01:02 +0300

    feat: write dummy implementation

--- examples/hello.rs
+++ examples/hello.rs
@@ -4,13 +4,13 @@ use bitcask::{Bitcask, OpenOptions};
 use bytes::Bytes;
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let bitcask = Bitcask::open("data", OpenOptions::ReadWrite)?;
+    let mut bitcask = Bitcask::open("data", OpenOptions::ReadWrite)?;
 
     let hello = Bytes::from_static(b"hello");
     let world = Bytes::from_static(b"world");
 
     bitcask.put(hello.clone(), world.clone())?;
-    assert_eq!(bitcask.get(&hello)?, world);
+    assert_eq!(bitcask.get(&hello)?, Some(world));
 
     Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
+use bincode::{Decode, Encode};
 use bytes::Bytes;
 use std::{
+    collections::HashMap,
     fs::{DirBuilder, File},
-    io,
-    path::Path,
+    io::{self, Seek},
+    os::unix::fs::FileExt,
+    path::{Path, PathBuf},
 };
 use thiserror::Error as ThisError;
 
@@ -16,29 +19,108 @@ pub enum OpenOptions {
 pub enum Error {
     #[error("io error")]
     Io(#[from] io::Error),
+
+    #[error("encode error")]
+    Encode(#[from] bincode::error::EncodeError),
+
+    #[error("decode error")]
+    Decode(#[from] bincode::error::DecodeError),
 }
 
 type Result<T> = std::result::Result<T, Error>;
 
 #[repr(C)]
-struct Log {
-    crc: u32,
-    timestamp: u32,
-    ksize: u32,
-    vsize: u32,
-    bytes: [u8],
+struct Log<T: AsRef<[u8]>> {
+    // TODO: Add CRC
+    timestamp: i64,
+    key: T,
+    value: Option<T>,
 }
 
-struct KeyDir {}
+impl<T: AsRef<[u8]>> Encode for Log<T> {
+    fn encode<E: bincode::enc::Encoder>(
+        &self,
+        encoder: &mut E,
+    ) -> std::result::Result<(), bincode::error::EncodeError> {
+        Encode::encode(&self.timestamp, encoder)?;
+        Encode::encode(self.key.as_ref(), encoder)?;
+        Encode::encode(&self.value.as_ref().map(AsRef::as_ref), encoder)?;
+        Ok(())
+    }
+}
+
+impl<Context, T> Decode<Context> for Log<T>
+where
+    T: From<Vec<u8>> + AsRef<[u8]>,
+{
+    fn decode<D: bincode::de::Decoder<Context = Context>>(
+        decoder: &mut D,
+    ) -> std::result::Result<Self, bincode::error::DecodeError> {
+        let timestamp = Decode::decode(decoder)?;
+        let key: Vec<u8> = Decode::decode(decoder)?;
+        let value: Option<Vec<u8>> = Decode::decode(decoder)?;
+
+        Ok(Self {
+            timestamp,
+            key: T::from(key),
+            value: value.map(T::from),
+        })
+    }
+}
+
+struct KeyDirEntry {
+    /// The path to the file datafile.
+    file_id: FileId,
+
+    // TODO: Store onnly the position of the value here.
+    /// The start position of the log.
+    pos: u64,
+
+    /// The size of the log.
+    size: u64,
+
+    /// The stored timestamp.
+    timestamp: i64,
+}
+
+struct KeyDir {
+    keys: HashMap<Bytes, KeyDirEntry>,
+}
+
+impl KeyDir {
+    fn new() -> Self {
+        Self {
+            keys: HashMap::new(),
+        }
+    }
+
+    fn get<T>(&self, key: T) -> Option<&KeyDirEntry>
+    where
+        T: AsRef<[u8]>,
+    {
+        self.keys.get(key.as_ref())
+    }
+
+    fn put(&mut self, key: Bytes, entry: KeyDirEntry) {
+        self.keys.insert(key, entry);
+    }
+
+    fn delete<T>(&mut self, key: T) -> Option<KeyDirEntry>
+    where
+        T: AsRef<[u8]>,
+    {
+        self.keys.remove(key.as_ref())
+    }
+}
 
 /// Represents a Bitcask handle.
 pub struct Bitcask {
     file: File,
+    file_id: FileId,
+    keydir: KeyDir,
 }
 
 impl Bitcask {
-    const ACTIVE_DATAFILE_NAME: &str = "active.bitcask.data";
-
     /// Open a new or existing Bitcask datastore with additional options.
     ///
     /// Valid options include read write (if this process is going to be a
@@ -50,15 +132,22 @@ impl Bitcask {
     pub fn open(dir: impl AsRef<Path>, opts: OpenOptions) -> Result<Self> {
         DirBuilder::new().recursive(true).create(dir.as_ref())?;
 
+        let file_id = FileId::new();
         let file = File::options()
             .read(true)
             .write(true)
             .create(true)
-            .open(dir.as_ref().join(Self::ACTIVE_DATAFILE_NAME))?;
+            .open(dir.as_ref().join(file_id.datafile_path()))?;
 
         file.lock()?;
 
-        let bitcask = Self { file };
+        let keydir = KeyDir::new();
+
+        let bitcask = Self {
+            file,
+            file_id,
+            keydir,
+        };
 
         Ok(bitcask)
     }
@@ -91,16 +180,99 @@ impl Bitcask {
         Ok(std::iter::empty())
     }
 
-    pub fn put(&self, key: Bytes, value: Bytes) -> Result<()> {
-        todo!();
+    pub fn put(&mut self, key: Bytes, value: Bytes) -> Result<()> {
+        let (size, pos, timestamp) = self.append(key.clone(), Some(value))?;
+
+        self.sync()?;
+
+        let entry = KeyDirEntry {
+            file_id: self.file_id,
+            timestamp,
+            pos,
+            size,
+        };
+
+        self.keydir.put(key, entry);
+
+        Ok(())
     }
 
-    pub fn get(&self, key: &[u8]) -> Result<Bytes> {
-        todo!()
+    pub fn get<T>(&mut self, key: T) -> Result<Option<Bytes>>
+    where
+        T: AsRef<[u8]>,
+    {
+        let Some(entry) = self.keydir.get(&key) else {
+            return Ok(None);
+        };
+
+        let file = if entry.file_id == self.file_id {
+            &mut self.file
+        } else {
+            &mut File::options()
+                .read(true)
+                .open(&entry.file_id.datafile_path())?
+        };
+
+        let mut bytes = vec![0; 4 * 30 + key.as_ref().len() + entry.size as usize];
+        file.read_at(&mut bytes, entry.pos)?;
+
+        let (log, _): (Log<Bytes>, usize) =
+            bincode::decode_from_slice(&bytes, bincode::config::standard())?;
+
+        Ok(log.value)
     }
 
-    pub fn delete(&self, key: &[u8]) -> Result<Bytes> {
-        todo!()
+    pub fn delete(&mut self, key: &[u8]) -> Result<()> {
+        let Some(_) = self.keydir.get(key) else {
+            return Ok(());
+        };
+
+        let _ = self.append(key, None)?;
+
+        self.keydir.delete(key);
+
+        Ok(())
+    }
+
+    pub fn append<T: AsRef<[u8]>>(&mut self, key: T, value: Option<T>) -> Result<(u64, u64, i64)> {
+        let pos = self.file.stream_position()?;
+        let timestamp = timestamp();
+
+        let log: Log<&[u8]> = Log {
+            key: key.as_ref(),
+            value: value.as_ref().map(AsRef::as_ref),
+            timestamp,
+        };
+
+        let size =
+            bincode::encode_into_std_write(log, &mut self.file, bincode::config::standard())?;
+
+        Ok((size as u64, pos, timestamp))
+    }
+}
+
+fn timestamp() -> i64 {
+    chrono::offset::Utc::now().timestamp()
+}
+
+/// A sortable file identifier.
+#[derive(Copy, Clone, Debug, PartialEq)]
+struct FileId {
+    id: i64,
+}
+
+impl FileId {
+    fn new() -> Self {
+        // TODO: Use UUIDv7 later
+        Self { id: timestamp() }
+    }
+
+    fn datafile_path(&self) -> PathBuf {
+        PathBuf::from(format!("{}.bitcask.data", self.id))
+    }
+
+    fn hintfile_path(&self) -> PathBuf {
+        PathBuf::from(format!("{}.bitcask.hint", self.id))
     }
 }
 
