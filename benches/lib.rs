8dce34492bbf5480fa4a422dd8cd899602e1e1c0
brkydnc <berkayydinc@gmail.com>
2025-10-30 23:32:25 +0300

    bench: write bench for put

--- benches/lib.rs
+++ benches/lib.rs
@@ -1,7 +1,30 @@
-use bitcask::{Bitcask, Error, OpenOptions};
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use bitcask::Bitcask;
+use bytes::Bytes;
+use criterion::{Criterion, criterion_group, criterion_main};
+use tempfile::tempdir;
 
-pub fn write_only_benchmark(c: &mut Criterion) {}
+fn bench_put(c: &mut Criterion) {
+    let mut group = c.benchmark_group("put");
 
-criterion_group!(benches, write_only_benchmark);
-criterion_main!(benches);
+    group.bench_function("constant_key_value_size", |b| {
+        let dir = tempdir().unwrap();
+        let mut bitcask = Bitcask::open(&dir, Default::default()).unwrap();
+
+        let key = Box::leak(vec![0u8; 64].into_boxed_slice());
+        let value = Box::leak(vec![0u8; 64].into_boxed_slice());
+
+        rand::fill(key);
+        rand::fill(value);
+
+        b.iter(|| {
+            bitcask
+                .put(Bytes::from_static(key), Bytes::from_static(value))
+                .unwrap()
+        });
+
+        std::fs::remove_dir_all(dir).unwrap();
+    });
+}
+
+criterion_group!(put, bench_put);
+criterion_main!(put);
